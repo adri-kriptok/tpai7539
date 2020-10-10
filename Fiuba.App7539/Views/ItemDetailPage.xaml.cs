50b9b152f02de58c49a72d510a42e7337c0200f7
Zankoku Kriptok <zank_kriptok@hotmail.com>
2020-10-10 19:01:29 -0300

    Primer commit.

---
+++ Fiuba.App7539/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Fiuba.App7539.ViewModels;
+
+namespace Fiuba.App7539.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Fiuba.App7539/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Fiuba.App7539.Models;
+using Fiuba.App7539.ViewModels;
+
+namespace Fiuba.App7539.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
