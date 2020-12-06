845d879a89f546ff4e0f172ea0ad54b526989b65
Zankoku Kriptok <zank_kriptok@hotmail.com>
2020-12-06 19:48:45 -0300

    Prueba.

--- Fiuba.App7539/Views/AboutPage.xaml.cs
+++ Fiuba.App7539/Views/AboutPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using System.ComponentModel;
+﻿using Shiny;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace Fiuba.App7539.Views
 {
@@ -11,7 +14,52 @@ namespace Fiuba.App7539.Views
         {
             InitializeComponent();
 
-            //var but = Content.FindByName<Button>("testButton");
+            var but = Content.FindByName<Button>("testButton");
+            but.Clicked += But_Clicked;
+        }
+
+        private async void But_Clicked(object sender, EventArgs e)
+        {
+            var locales = await TextToSpeech.GetLocalesAsync();
+
+            // Grab the first locale
+            var locale = locales.FirstOrDefault(l => l.Country.ToLower() == "es" && l.Language.ToLower() == "es");
+
+            var settings = new SpeechOptions()
+            {
+                Volume = .75f,
+                Pitch = 1.0f,
+                Locale = locale
+            };
+
+            await TextToSpeech.SpeakAsync("Hola Popa", settings);
+
+            //Shiny.ShinySpeechRecognizer
+            //ShinySpeechRecognizer.Current
+            //await TextToSpeech.SpeakAsync("Hello World");
+
+            //TextToSpeech.SpeakAsync("Hello World").ContinueWith((t) =>
+            //{
+            //    // Logic that will run after utterance finishes.
+            //
+            //}, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //var acc = ShinySpeechRecognizer.RequestAccess().Result;
+            //
+            //if (acc == AccessState.Available)
+            //{
+            //    ShinySpeechRecognizer.ListenUntilPause().Subscribe(phrase =>
+            //    {
+            //        Trace.WriteLine(phrase);
+            //    });
+            //}
+            //.Current
+            //.ListenUntilPause()
+            //.Subscribe(phrase =>
+            //{
+            //
+            //});               
+
         }
     }
 }
